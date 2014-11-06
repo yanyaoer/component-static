832538a49302babd2b9a862418b2d25a77870b82
nealnote <nealnote@gmail.com>
2014-11-06 10:27:09 +0800

    tpl/builder.js exec anywhere

--- source/tpl/builder.js
+++ source/tpl/builder.js
@@ -3,11 +3,12 @@ var fs = require('fs')
 var write = fs.writeFileSync;
 var jade = require('jade');
 
-var file = '../tpl.js';
+var target = path.join(__dirname, '../tpl.js');
 
 fs.readdir(__dirname, function(err, files){
-  var res = []
+  var res = [];
   files.forEach(function(file){
+    var file = path.join(__dirname, file);
     var ext = path.extname(file);
     if ('.jade' != ext) return;
 
@@ -17,5 +18,5 @@ fs.readdir(__dirname, function(err, files){
 		res.push('"'+path.basename(file, '.jade') + '": ' + fn.toString());
   })
 	//console.log(res)
-	write(file, 'var jade = require("jade"); module.exports = { ' + res.join(", ") + '};')
+	write(target, 'var jade = require("jade"); module.exports = { ' + res.join(", ") + '};')
 })
